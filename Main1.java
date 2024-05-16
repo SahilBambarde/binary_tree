d1f249f327c760f9a2d3be1a0cd4dd3c9107fce6
sahil_bambarde <sahil_bambarde>
2024-05-17 01:49:29 +0530

    Main Commit

--- Main1.java
+++ Main1.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main1 {
+public class Main1 {
+    // Reverse an array
+    public static void main(String agrs[]){
+        int[] a ={0,1,3,4,5,6};
+        a = reverse(a);
+        for(int i: a){
+            System.out.print(i+" ");
+        }
+
+    }
+
+    static int[] reverse(int[] a){
+        int i = 0;
+        int j =  a.length-1;
+        while(i<j && i!=j){
+            int x = a[i];
+            a[i]=a[j];
+            a[j]=x;
+            i++;
+            j--;
+        }
+        return a;
+    }
 }
